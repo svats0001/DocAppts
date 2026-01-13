73a12f103f5b807bc51d6e5849aad2950d6d6450
svats0001 <vats02581@gmail.com>
2026-01-13 15:55:14 +1100

    Delete PracticeMS/PracticeMS directory

--- PracticeMS/PracticeMS/src/main/java/com/docappts/PracticeMS/repository/KeyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.docappts.PracticeMS.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.docappts.PracticeMS.entity.ApiKey;
-
-public interface KeyRepository extends JpaRepository<ApiKey, String> {
-    
-}
