546821f9121e759b10f37722ddde2040f93e6f27
svats0001 <vats02581@gmail.com>
2026-01-13 15:55:02 +1100

    Delete AppointmentMS/AppointmentMS directory

--- AppointmentMS/AppointmentMS/src/main/java/com/docappts/AppointmentMS/repository/KeyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.docappts.AppointmentMS.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.docappts.AppointmentMS.entity.ApiKey;
-
-public interface KeyRepository extends JpaRepository<ApiKey, String> {
-    
-}
