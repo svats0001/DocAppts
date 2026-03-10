5a39c020d3537ae25110e2aa0a0e7a69079ac868
svats0001 <vats02581@gmail.com>
2026-03-11 10:05:15 +1100

    Delete PaymentsMS/PaymentsMS directory

--- PaymentsMS/PaymentsMS/src/main/java/com/docappts/PaymentsMS/repository/KeyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.docappts.PaymentsMS.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.docappts.PaymentsMS.entity.ApiKey;
-
-public interface KeyRepository extends JpaRepository<ApiKey, String> {
-    
-}
