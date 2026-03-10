e9181585e080419189d78e03c93afa512a413a96
svats0001 <vats02581@gmail.com>
2026-03-11 10:04:27 +1100

    Delete UserMS/UserMS directory

--- UserMS/UserMS/src/main/java/com/docappts/UserMS/repository/KeyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.docappts.UserMS.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.docappts.UserMS.entity.ApiKey;
-
-public interface KeyRepository extends JpaRepository<ApiKey, String> {
-    
-}
--- UserMS/UserMS/src/main/java/com/docappts/UserMS/repository/SessionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.docappts.UserMS.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.docappts.UserMS.entity.UserSession;
-
-public interface SessionRepository extends JpaRepository<UserSession, String> {
-    
-}
