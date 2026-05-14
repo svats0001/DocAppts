46e5dfbc91e84f48b147cc4946cbe3f20cd87157
svats0001 <vats02581@gmail.com>
2026-05-14 13:08:52 +1000

    Delete PracticeMS/PracticeMS directory

--- PracticeMS/PracticeMS/src/main/java/com/docappts/PracticeMS/repository/PracticeSessionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.docappts.PracticeMS.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.docappts.PracticeMS.entity.PracticeSession;
-
-public interface PracticeSessionRepository extends JpaRepository<PracticeSession, String> {
-    
-}
