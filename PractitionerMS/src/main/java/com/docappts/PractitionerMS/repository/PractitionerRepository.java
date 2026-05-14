b67e32498f6e05a609d14c1db3da883afd15689f
svats0001 <vats02581@gmail.com>
2026-05-14 13:37:30 +1000

    Add files via upload

---
+++ PractitionerMS/src/main/java/com/docappts/PractitionerMS/repository/PractitionerRepository.java
@@ -0,0 +1,13 @@
+package com.docappts.PractitionerMS.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.docappts.PractitionerMS.entity.Practitioner;
+
+public interface PractitionerRepository extends JpaRepository<Practitioner, Integer> {
+    Optional<Practitioner> findByEmailAndMobile(String email, String mobile);
+
+    Optional<Practitioner> findByPublicId(String publicId);
+}
