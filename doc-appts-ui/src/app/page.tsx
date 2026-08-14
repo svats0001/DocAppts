c4f7e7d65b8d8cb8d11547860930d80c38aca7e8
svats0001 <vats02581@gmail.com>
2026-08-14 16:04:54 +1000

    Delete doc-appts-ui directory

--- doc-appts-ui/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Search from "./search/search";
-
-export default function Home() {
-  return (
-    <div>
-      <Search practices={[]} err={false}></Search>
-    </div>
-  );
-}
--- doc-appts-ui/src/app/practice/passwordReset/success/page.tsx
+++
@@ -1,9 +0,0 @@
-import styles from '@/app/practice/practice.module.css';
-
-export default function Page() {
-    return (
-        <div className={styles.practiceInfo}>
-            <p>Successfully reset password</p>
-        </div>
-    )
-}
