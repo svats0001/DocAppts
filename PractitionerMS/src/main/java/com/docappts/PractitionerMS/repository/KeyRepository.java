275aa79cd8ccad3c9c13e632ec335b0d493de475
svats0001 <vats02581@gmail.com>
2026-08-14 16:04:34 +1000

    Delete PractitionerMS directory

--- PractitionerMS/src/main/java/com/docappts/PractitionerMS/repository/KeyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.docappts.PractitionerMS.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.docappts.PractitionerMS.entity.ApiKey;
-
-public interface KeyRepository extends JpaRepository<ApiKey, String> {
-    
-}
